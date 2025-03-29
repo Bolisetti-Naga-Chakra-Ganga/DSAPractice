78a54655f5b516e11ca7956597663085268ac88f
Gangabolisetti <nagachakraganga@gmail.com>
2025-03-29 16:43:16 +0530

    2 new files adde

---
+++ intro/varibles.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a=243;
+    cout<< a << endl;
+    char b='v';
+    cout<< b<< endl;
+    bool bl=true;
+    cout<< bl<< endl;
+    float f=1.2;
+    cout<<f<< endl;
+    double d = 1.23;
+    cout<<d<< endl;
+    int si=sizeof(d);
+    cout<<si<<endl;
+    int t='a';
+    cout<<a<<endl;
+    char k=97;
+    }
--- pract.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-#include<array>
-#include<vector>
-using namespace std;
- int main(){
-   cout<< 12%32;
- }
